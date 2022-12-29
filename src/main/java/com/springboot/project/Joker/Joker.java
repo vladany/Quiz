bc4b73ddbad8fedb5c082168cf012b9fd7333ec4
Gabriel <gabriel.barocksei@stud.fra-uas.de>
2022-12-29 12:00:28 +0100

    @Gabriel //

--- src/main/java/com/springboot/project/Joker/Joker.java
+++ src/main/java/com/springboot/project/Joker/Joker.java
@@ -1,7 +1,10 @@
 package com.springboot.project.Joker;
 
+import com.springboot.project.Game.Game;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Joker {
@@ -9,5 +12,45 @@ public class Joker {
     @Id
     private Integer id;
 
+    private String jokerTyp;
+
+    private Boolean used;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+   public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getJokerTyp() {
+        return jokerTyp;
+    }
+
+    public void setJokerTyp(String jokerTyp) {
+        this.jokerTyp = jokerTyp;
+    }
+
+    public Boolean getUsed() {
+        return used;
+    }
+
+    public void setUsed(Boolean used) {
+        this.used = used;
+    }
+
+    @Override
+    public String toString() {
+        return "Joker{" +
+                "id=" + id +
+                ", jokerTyp='" + jokerTyp + '\'' +
+                ", used=" + used +
+                '}';
+    }
 
 }
--- src/main/java/com/springboot/project/Joker/JokerService.java
+++ src/main/java/com/springboot/project/Joker/JokerService.java
@@ -1,7 +1,30 @@
 package com.springboot.project.Joker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokerService {
+
+    @Autowired
+    private JokerRepository repo;
+
+    public void addNewJoker(Joker joker){
+        repo.save(joker);
+    }
+
+    public Joker findeJokerById(Integer id){
+        return repo.findById(id).get();
+    }
+
+    public void deleteRoleById(Integer id) {
+        repo.deleteById(id);
+    }
+
+
+    public void initJokers(){
+
+        Joker
+    }
+
 }

45ba74746809c7046e6fb74e54257129dacfafc5
varzrelian <vlada.azrelian@qytera.de>
2023-02-26 20:31:04 +0100

    update

---
+++ src/main/java/com/example/QuizProject/quiz/level2/QuizController2.java
@@ -0,0 +1,92 @@
+package com.example.QuizProject.quiz;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuizController {
+
+
+    double getRandomQuestionId = 1 + (Math.random() * 5);
+    int i = (int)getRandomQuestionId;
+    @Autowired
+    QuizService quizService;
+
+    @PostConstruct
+    private void postInit() {
+        System.out.println("Initialized");
+        quizService.createQuestion();
+    }
+
+    @GetMapping("/check")
+    @ResponseBody
+    public String check(@RequestParam String name) {
+
+        // hier muss random passieren
+
+        quizService.getQuiz(2);
+        String nameCheck = quizService.getQuiz(2).getOptionRight();
+        System.out.println("Got the request");
+        if (name.equalsIgnoreCase(nameCheck)){
+            return "Korrekt";
+        }
+        return "Falsch";
+    }
+
+    @GetMapping("/check2")
+    @ResponseBody
+    public String check2(@RequestParam String name) {
+
+        // hier muss random passieren
+
+        quizService.getQuiz(4);
+        String nameCheck = quizService.getQuiz(4).getOptionRight();
+        System.out.println("Got the request");
+        if (name.equalsIgnoreCase(nameCheck)){
+            return "Korrekt";
+        }
+        return "Falsch";
+    }
+
+    @GetMapping("/check3")
+    @ResponseBody
+    public String check3(@RequestParam String name) {
+
+        // hier muss random passieren
+
+        quizService.getQuiz(6);
+        String nameCheck = quizService.getQuiz(6).getOptionRight();
+        System.out.println("Got the request");
+        if (name.equalsIgnoreCase(nameCheck)){
+            return "Korrekt";
+        }
+        return "Falsch";
+    }
+
+    @GetMapping("quiz")
+    @ResponseBody
+    public Quiz quiz(@RequestParam(value = "quiz", required = false, defaultValue = "") Object term) {
+        Quiz quiz = quizService.getQuiz(2);
+        return quiz;
+    }
+
+    @GetMapping("quiz2")
+    @ResponseBody
+    public Quiz quiz2(@RequestParam(value = "quiz2", required = false, defaultValue = "") Object term) {
+        Quiz quiz2 = quizService.getQuiz(4);
+        return quiz2;
+    }
+
+    @GetMapping("quiz3")
+    @ResponseBody
+    public Quiz quiz3(@RequestParam(value = "quiz3", required = false, defaultValue = "") Object term) {
+        Quiz quiz3 = quizService.getQuiz(6);
+        return quiz3;
+    }
+}
---
+++ src/main/java/com/example/QuizProject/quiz/level2/QuizService2.java
@@ -0,0 +1,56 @@
+package com.example.QuizProject.quiz;
+
+import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class QuizService {
+
+
+    @Autowired
+    private QuizRepository quizRepository;
+    @Autowired
+    private Quiz quiz;
+    public void save(Quiz quiz) {
+
+        quizRepository.save(quiz);
+    }
+
+    public Quiz getQuiz(Integer id) {
+        Quiz quiz = quizRepository.findById(id).get();
+        return quiz;
+    }
+
+    public void createQuestion() {
+
+        ArrayList<Quiz> frage = new ArrayList<>();
+        Quiz frage1 = new Quiz(1, "Wie viele Bundeslänger hat Deutschland", "16", "6", "24", "d");
+        Quiz frage2 = new Quiz(2, "Was ist der höchste Berg?", "Mount Everest", "Zugspitze", "Mont Blanc", "Dolomiten");
+        Quiz frage3 = new Quiz(3, "Wie nennt man ein männliches Schwein?", "Eber", "Erpel", "Bock", "Hengst");
+        Quiz frage4 = new Quiz(4, "Wofür stehen die olympischen Ringe?", "Kontinente", "Disziplinen", "Jahreszeiten", "Frieden");
+        Quiz frage5 = new Quiz(5, "Welcher Planet ist der Sonne am nähsten?", "Merkur", "Jupiter", "Mars", "Uranus");
+        Quiz frage6 = new Quiz(6, "Was versteht man unter einem String?", "Zeichenkette", "Zahlenkette", "Datenkette", "Wahrheitswert");
+
+        frage.add(frage1);
+        frage.add(frage2);
+        frage.add(frage3);
+        frage.add(frage4);
+        frage.add(frage5);
+        frage.add(frage6);
+
+        quizRepository.save(frage1);
+        quizRepository.save(frage2);
+        quizRepository.save(frage3);
+        quizRepository.save(frage4);
+        quizRepository.save(frage5);
+        quizRepository.save(frage6);
+
+    }
+}
+

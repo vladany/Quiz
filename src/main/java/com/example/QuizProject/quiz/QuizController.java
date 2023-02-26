2573e353f3de875a50380c4c30e187078feff471
varzrelian <vlada.azrelian@qytera.de>
2023-02-26 21:44:24 +0100

    randomize questions

--- src/main/java/com/example/QuizProject/quiz/QuizController.java
+++ src/main/java/com/example/QuizProject/quiz/QuizController.java
@@ -13,8 +13,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class QuizController {
 
 
-    double getRandomQuestionId = 1 + (Math.random() * 5);
-    int i = (int)getRandomQuestionId;
+    double getRandomQuestionId1 = 1 + (Math.random() * 4);
+    int level1 = (int)getRandomQuestionId1;
+
+
+    double getRandomQuestionId2 = 4 + (Math.random() * 9);
+    int level2 = (int)getRandomQuestionId2;
+
+
+    double getRandomQuestionId3 = 9 + (Math.random() * 14);
+    int level3 = (int)getRandomQuestionId3;
+
     @Autowired
     QuizService quizService;
 
@@ -24,29 +33,39 @@ public class QuizController {
         quizService.createQuestion();
     }
 
+
+    @GetMapping("quiz")
+    @ResponseBody
+    public Quiz quiz(@RequestParam(value = "quiz", required = false, defaultValue = "") Object term) {
+        Quiz quiz = quizService.getQuiz(level1);
+        return quiz;
+    }
+
+    @GetMapping("quiz2")
+    @ResponseBody
+    public Quiz quiz2(@RequestParam(value = "quiz2", required = false, defaultValue = "") Object term) {
+        Quiz quiz2 = quizService.getQuiz(level2);
+        return quiz2;
+    }
+
+
+
+    @GetMapping("quiz3")
+    @ResponseBody
+    public Quiz quiz3(@RequestParam(value = "quiz3", required = false, defaultValue = "") Object term) {
+        Quiz quiz3 = quizService.getQuiz(level3);
+        return quiz3;
+    }
+
+
     @GetMapping("/check")
     @ResponseBody
     public String check(@RequestParam String name) {
 
         // hier muss random passieren
 
-        quizService.getQuiz(1);
-        String nameCheck = quizService.getQuiz(1).getOptionRight();
-        System.out.println("Got the request");
-        if (name.equalsIgnoreCase(nameCheck)){
-            return "Korrekt";
-        }
-        return "Falsch";
-    }
-
-    @GetMapping("/check3")
-    @ResponseBody
-    public String check3(@RequestParam String name) {
-
-        // hier muss random passieren
-
-        quizService.getQuiz(3);
-        String nameCheck = quizService.getQuiz(3).getOptionRight();
+        quizService.getQuiz(level1);
+        String nameCheck = quizService.getQuiz(level1).getOptionRight();
         System.out.println("Got the request");
         if (name.equalsIgnoreCase(nameCheck)){
             return "Korrekt";
@@ -61,8 +80,8 @@ public class QuizController {
 
         // hier muss random passieren
 
-        quizService.getQuiz(2);
-        String nameCheck = quizService.getQuiz(2).getOptionRight();
+        quizService.getQuiz(level2);
+        String nameCheck = quizService.getQuiz(level2).getOptionRight();
         System.out.println("Got the request");
         if (name.equalsIgnoreCase(nameCheck)){
             return "Korrekt";
@@ -70,25 +89,19 @@ public class QuizController {
         return "Falsch";
     }
 
-    @GetMapping("quiz2")
+    @GetMapping("/check3")
     @ResponseBody
-    public Quiz quiz2(@RequestParam(value = "quiz2", required = false, defaultValue = "") Object term) {
-        Quiz quiz2 = quizService.getQuiz(2);
-        return quiz2;
+    public String check3(@RequestParam String name) {
+
+        // hier muss random passieren
+
+        quizService.getQuiz(level3);
+        String nameCheck = quizService.getQuiz(level3).getOptionRight();
+        System.out.println("Got the request");
+        if (name.equalsIgnoreCase(nameCheck)){
+            return "Korrekt";
+        }
+        return "Falsch";
     }
 
-    @GetMapping("quiz")
-    @ResponseBody
-    public Quiz quiz(@RequestParam(value = "quiz", required = false, defaultValue = "") Object term) {
-        Quiz quiz = quizService.getQuiz(1);
-        return quiz;
-    }
-
-
-    @GetMapping("quiz3")
-    @ResponseBody
-    public Quiz quiz3(@RequestParam(value = "quiz3", required = false, defaultValue = "") Object term) {
-        Quiz quiz3 = quizService.getQuiz(3);
-        return quiz3;
-    }
 }

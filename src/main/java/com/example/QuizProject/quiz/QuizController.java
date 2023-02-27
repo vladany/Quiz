0bb94a31ca95ce59261433e5b31a99f000b2156d
varzrelian <vlada.azrelian@qytera.de>
2023-02-27 21:57:17 +0100

    restcontroller

--- src/main/java/com/example/QuizProject/quiz/QuizController.java
+++ src/main/java/com/example/QuizProject/quiz/QuizController.java
@@ -4,12 +4,9 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class QuizController {
 
     int minLevel1 = 1;
